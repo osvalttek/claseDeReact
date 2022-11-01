0788faaf4e424ebba33edfaf6d484135d40a6812
Osvaldito <osvaldo.devmate@gmail.com>
2022-10-31 21:13:54 -0300

    create react app, web pack, babel, modularizacion, jsx, componentes

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      Header
+      <nav>
+        <a href="">link</a>
+        <a href="">link</a>
+        <a href="">link</a>
+        <a href="">link</a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
