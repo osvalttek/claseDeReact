4c071f9c9fb1e4604c3dbedfd98dda3b65819e7b
ovaldo ojeda <osvaldoojeda@Mac-mini-de-Osvaldo.local>
2022-12-07 19:18:40 -0300

    delete de quoteSlice

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,14 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { simpsonApi } from "./api/quoteApi";
-
 // aca llamamos a nuestros reducers
 import counterReducer from "./slices/counterSlice";
-import quotesReducer from "./slices/quotesSlice";
+import { simpsonApi } from "./api/quoteApi";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
-    quotes: quotesReducer,
     [simpsonApi.reducerPath]: simpsonApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
